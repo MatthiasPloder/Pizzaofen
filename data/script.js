18ca52c922384cabb0513de24e6be1903abeb0ce
MatthiasPloder <plodermatthias@gmail.com>
2024-04-19 09:44:53 +0200

    everything

---
+++ data/script.js
@@ -0,0 +1,89 @@
+var tempereture;
+var oben;
+
+function adjustHeating(temperature) {
+  console.log("Heizelement auf " + temperature + " Grad einstellen.");
+  document.getElementById("current-temperature").innerHTML = temperature + " Grad Celsius";
+  tempereture = temperature;
+}
+
+function setPizzaTemperature(temperature) {
+  adjustHeating(temperature);
+  document.getElementById("heating-range").value = temperature;
+}
+// GRAD || unten always on  OBEN = 0/1 
+function startBacking() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/update?output=" + tempereture + "/" + oben, true);
+  xhr.send();
+}
+
+function toggleHeating(heatingType) {
+  let upperHeatingStatus = document.getElementById("upper-heating-status");
+  let lowerHeatingStatus = document.getElementById("lower-heating-status");
+  let upperHeatingOverlay = document.getElementById("pizzaofen-overlay-upper");
+  let lowerHeatingOverlay = document.getElementById("pizzaofen-overlay-lower");
+  let bothHeatingOverlay = document.getElementById("pizzaofen-overlay-both");
+
+  if (heatingType === 'upper') {
+      if (upperHeatingStatus.innerHTML === "Aus") {
+        oben = 1;
+          upperHeatingStatus.innerHTML = "An";
+          upperHeatingOverlay.style.display = "block"; // Overlay anzeigen
+          console.log("Obere Heizung eingeschaltet.");
+      } else {
+        oben = 0;
+          upperHeatingStatus.innerHTML = "Aus";
+          upperHeatingOverlay.style.display = "none"; // Overlay ausblenden
+          console.log("Obere Heizung ausgeschaltet.");
+      }
+  } else if (heatingType === 'lower') {
+      if (lowerHeatingStatus.innerHTML === "Aus") {
+          lowerHeatingStatus.innerHTML = "An";
+          lowerHeatingOverlay.style.display = "block"; // Overlay anzeigen
+          console.log("Untere Heizung eingeschaltet.");
+      } else {
+          lowerHeatingStatus.innerHTML = "Aus";
+          lowerHeatingOverlay.style.display = "none"; // Overlay ausblenden
+          console.log("Untere Heizung ausgeschaltet.");
+      }
+  }
+
+  // Überprüfen, ob beide Heizelemente eingeschaltet sind und das entsprechende Overlay anzeigen
+  if (upperHeatingStatus.innerHTML === "An" && lowerHeatingStatus.innerHTML === "An") {
+      bothHeatingOverlay.style.display = "block";
+  } else {
+      bothHeatingOverlay.style.display = "none";
+  }
+}
+
+function turnOffAllHeating() {
+  tempereture = 0;
+  oben = 2;     // 2 = alles aus
+  let heatingTypes = ["upper", "lower"];
+  heatingTypes.forEach(function(heatingType) {
+      document.getElementById(heatingType + "-heating-status").innerHTML = "Aus";
+      document.getElementById("pizzaofen-overlay-" + heatingType).style.display = "none";
+  });
+  document.getElementById("current-temperature").innerHTML = "0 Grad Celsius";
+  document.getElementById("heating-range").value = 0;
+  document.getElementById("pizzaofen-overlay-both").style.display = "none"; // Overlay für beide Heizungen ausblenden
+  console.log("Alle Heizungen ausgeschaltet.");
+}
+
+function toggleGrill() {
+  turnOffAllHeating();
+
+  let lowerHeatingStatus = document.getElementById("lower-heating-status");
+  let lowerHeatingOverlay = document.getElementById("pizzaofen-overlay-lower");
+
+      lowerHeatingStatus.innerHTML = "An";
+      lowerHeatingOverlay.style.display = "block"; // Overlay anzeigen
+      console.log("Untere Heizung (Grill) eingeschaltet.");
+      setPizzaTemperature(170); // Temperatur auf 170 Grad setzen
+  
+}
+
+window.onload = function() {
+  turnOffAllHeating();
+};
---
+++ src/ESP32_Async_Web_Server.ino
@@ -0,0 +1,110 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
+#include <Wire.h>
+
+const char *ssid = "matze";
+const char *password = "123456789";
+const int serverPort = 80;
+
+int x = 0;
+int i = 0;
+int Temparatur[8];
+
+#define NODE_ADDRESS 2
+#define PAYLOAD_SIZE 2
+byte nodePayload[PAYLOAD_SIZE];
+
+AsyncWebServer server(serverPort);
+
+void setup()
+{
+  Serial.begin(9600);
+
+  Wire.begin(NODE_ADDRESS);
+  Wire.onReceive(receiveEvent); // erstelle ein Empfangen-Ereignis
+  Wire.onRequest(requestEvent); // erstelle ein Anfrage-Ereignis
+  connectToWifi();
+
+  // Mount SPIFFS
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/mainSite.html", "text/html"); });
+
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String output = request->getParam("output")->value();
+    int temperature = output.substring(0, output.indexOf("/")).toInt();
+    int oben = output.substring(output.indexOf("/") + 1).toInt();
+    // Process the received data here
+    Serial.println("Received data: temperature = " + String(temperature) + ", oben = " + String(oben));
+    request->send(200, "text/plain", "Data received successfully");
+  });
+  
+  server.on("/styles.css", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/styles.css", "text/css"); });
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/script.js", "text/javascript"); });
+  server.on("/img/pizzaofen.jpeg", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/img/pizzaofen.jpeg", "image/jpeg"); });
+  server.on("/img/IMG_1494.jpeg", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/img/IMG_1494.jpeg", "image/jpeg"); });
+  server.on("/img/definition-von-feuer.jpg", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/img/definition-von-feuer.jpg", "image/jpeg"); });
+  server.on("/img/Oben.jpeg", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/img/Oben.jpeg", "image/jpeg"); });
+  server.on("/img/Unten.jpeg", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(SPIFFS, "/img/Unten.jpeg", "image/jpeg"); });
+
+  server.begin();
+}
+
+String processor(const String &var)
+{
+
+  Serial.println(var);
+  return var;
+}
+
+void loop()
+{
+}
+
+void connectToWifi()
+{
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void receiveEvent(int bytes)
+{
+  x = Wire.read(); // lies die gesendeten Daten aus
+  i++;
+  Temparatur[i] = x * 2; // speichert die empfangenen Werte in einem Array ab
+  Serial.println(Temparatur[i]);
+  if (i > 8)
+  {
+    i = 0;
+  }
+}
+
+void requestEvent()
+{
+  nodePayload[0] = NODE_ADDRESS;
+  nodePayload[1] = analogRead(A0) / 4;
+  Wire.write(nodePayload, PAYLOAD_SIZE);
+}
